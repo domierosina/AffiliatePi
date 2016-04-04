b46af4994ac407cda558e66f7635c7b342260539
RobertH <rob3rthunt3r@gmail.com>
2016-04-04 00:35:25 -0500

    done?

--- server/main.js
+++ server/main.js
@@ -1,15 +1,15 @@
-Meteor.startup(function () {
-if(Meteor.users.find().count() === 0) {
-
-    console.log('Created Admin user');
-
-    Accounts.createUser({
-        username: 'admin',
-        email: 'admin@example.com',
-        password: 'password',
-        profile: {
-            name: 'Administrator'
-        }
-    });
-}
-});
+//Meteor.startup(function () {
+//if(Meteor.users.find().count() === 0) {
+//
+//    console.log('Created Admin user');
+//
+//    Accounts.createUser({
+//        username: 'admin',
+//        email: 'admin@example.com',
+//        password: 'password',
+//        profile: {
+//            name: 'Administrator'
+//        }
+//    });
+//}
+//});
